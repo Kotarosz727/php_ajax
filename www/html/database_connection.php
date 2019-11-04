e2960ddfba930d12446bf3d6fe2c2e0e18608164
Kotaro Suzuki <kotarosz727@gmail.com>
2019-11-04 16:06:39 +0900

    create insert_chat.php

--- www/html/database_connection.php
+++ www/html/database_connection.php
@@ -19,4 +19,53 @@ function fetch_user_last_activity($user_id, $connect){
   }
 }
 
+function fetch_user_chat_history($from_user_id, $to_user_id, $connect){
+  $query = "
+    SELECT * FROM chat_message
+    WHERE (from_user_id = '".$from_user_id."'
+    AND to_user_id = '".$to_user_id."')
+    OR (from_user_id = '".$to_user_id."'
+    AND to_user_id = '".$from_user_id."')
+    ORDER BY timestamp DESC
+  ";
+  echo $query;
+  $statement = $connect->prepare($query);
+  $statement -> execute();
+  $result = $statement->fetchALL();
+  $output = '<ul class="list-unstyled">';
+  foreach($result as $row){
+    $user_name = '';
+    if($row["from_user_id"] == $from_user_id){
+      $user_name = '<b classs="text-success">You</b>';
+
+    }else{
+      $user_name = '<b class="text-danger">'.get_user_name($row['from_user_id'], $connect).'</b>';
+    }
+    $output .= '
+    <li style="border-bottom:1px dotted #ccc">
+      <p>'.$user_name.' - '.$row["chat_message"].'
+        <div align="right">
+          - <small><em>'.$row['timestamp'].'</em></small>
+        </div>
+      </p>
+    </li>
+    ';
+    
+  }
+  $output .= '</ul>';
+  return $output;
+  }
+
+  function get_user_name($user_id, $connect){
+    $query = "SELECT username FROM login where user_id= '$user_id'";
+    $statement = $connect->prepare($query);
+    $statement->execute();
+    $result = $statement->fetchALL();
+    foreach($result as $row){
+      return $row['username'];
+    }
+  }
+
+
+
 ?>
--- www/html/index.php
+++ www/html/index.php
@@ -27,6 +27,7 @@ if(!isset($_SESSION['user_id']))
         <h4 align="center">Online User</h4>
         <p align="right">Hi - <?php echo $_SESSION['username']; ?> - <a href="logout.php">Logout</a></p>
         <div id="user_details"></div>
+        <div id="user_modal_details"></div>
       </div>
     </div>
   </body>  
@@ -62,6 +63,46 @@ if(!isset($_SESSION['user_id']))
         }
       })
     }
+
+    //チャットボックス関数
+    function make_chat_dialog_box(to_user_id, to_user_name){
+      var modal_content = '<div id="user_dialog_'+to_user_id+'" class="user_dialog" title="you have chat with'+to_user_name+'">';
+      modal_content += '<div style="height:400px; border:1px solid #ccc; overflow-y: scroll; margin-bottom:24px; padding:16px;" class="chat_history" data-touserid="'+to_user_id+'" id="chat_history_'+to_user_id+'">';
+      modal_content += '</div>';
+      modal_content += '<div class="form-group">';
+      modal_content += '<textarea name="chat_message_'+to_user_id+'" id="chat_message_'+to_user_id+'" class="form_control"></textarea>';
+      modal_content += '</div><div class="form-group" align="right">';
+      modal_content += '<button type="button" name="send_chat" id="'+to_user_id+'" class="btn btn-info send_chat">Send</button></div></div>';
+      $('#user_modal_details').html(modal_content);
+    }
+
+    $(document).on('click', '.start_chat', function(){
+    
+      var to_user_id = $(this).data('touserid');
+      var to_user_name = $(this).data('tousername');
+     
+      make_chat_dialog_box(to_user_id, to_user_name);
+      $("#user_dialog_"+to_user_id).dialog({
+        autoOpen:false,
+        width:400
+      });
+      $('#user_dialog_'+to_user_id).dialog('open');
+    });
+
+    $(document).on('click', '.send_chat', function(){
+      var to_user_id = $(this).attr('id');
+      var chat_message = $('#chat_message_'+to_user_id).val();
+      
+      $.ajax({
+        url:"insert_chat.php",
+        method:"POST",
+        data: {to_user_id:to_user_id, chat_message:chat_message},
+        success:function(data){
+          $('#chat_message_'+to_user_id).val('');
+          $('#chat_history_'+to_user_id).html(data);
+        }
+      }); 
+    });
 
   });
 </script>
